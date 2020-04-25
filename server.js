4b6ce2f428e421c57997b1ad4a74c08c634422b5
Celeste Lewis <cdlewis42@gmail.com>
2020-04-25 19:26:19 -0400

    scrape is working but has nowhere to render

--- server.js
+++ server.js
@@ -34,6 +34,24 @@ app.get("/scrape", function (req,res){
     axios.get("https://www.theonion.com/").then(function(response){
         //saving cheerio to variable for shorthand
         var $ = cheerio.load(response.data)
+
+        $("article h2").each(function(i,element){
+            var result = {};
+            result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("href")
+
+            db.Article.create(result)
+            .then(function(dbArticle){
+                console.log(dbArticle);
+            }).catch(function(err){
+                console.log(err)
+            })
+        })
+        res.send("Scrape Complete")
     })
 })
 
