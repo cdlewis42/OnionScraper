bcfa32361c705966aea33c3884be0fc16bd7c587
Celeste Lewis <cdlewis42@gmail.com>
2020-04-29 23:23:42 -0400

    api routes are in their own document now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,53 +3,47 @@ var cheerio = require("cheerio");
 var db = require("../models")
 
 //get route to scrape news site
+
 function apiRoutes(app) {
-    app.get("/scrape", function (req, res) {
-        axios.get("https://www.theonion.com/").then(function (response) {
-            //saving cheerio to variable for shorthand
-            var $ = cheerio.load(response.data)
-            var result = []
-            $("div.element").each(function (i, element) {
-                var title = $(element).find("a").text().trim()
-                var link = $(element).find("a").attr("href")
-                result.push({
-                    title,
-                    link
-                })
-                console.log("title")
-                console.log("link")
-                
-            // $("article h2").each(function (i, element) {
-            //     var title = $(element).find("a").text().trim()
-            //     var link = $(element).find("a").attr("href")
-            //     console.log(title)
-            //     console.log(link)
-            db.Article.create(result)
-                     .then(function (dbArticle) {
-                         // View the added result in the console
-                         console.log(dbArticle);
-                     })
-                     .catch(function (err) {
-                         // If an error occurred, log it
-                         console.log(err);
-                     });
-             });
+app.get("/", function (req,res){
+axios.get("https://www.theonion.com/").then(function(response) {
 
-            res.send("Scrape Complete")
-            console.log(dbArticle)
-        })
+  // Load the HTML into cheerio and save it to a variable
+  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+  // An empty array to save the data that we'll scrape
+    var results = [];
 
+  // Select each element in the HTML body from which you want information.
+  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+  // but be sure to visit the package's npm page to see how it works
+    $("article").each(function(i, element) {
+
+    var title = $(element).children().text();
+    var link = $(element).find("a").attr("href");
+
+    // Save these results in an object that we'll push into the results array we defined earlier
+    results.push({
+      title,
+      link
+    });
+    console.log(results)
+    // Create a new Article using the `result` object built from scraping
+    db.Article.create(results)
+      .then(function(dbArticle) {
+        // View the added result in the console
+        //console.log("this is dbArticle " +dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, log it
+        console.log(err);
+      });
+  });
+
+  // Log the results once you've looped through each of the elements found with cheerio
+  console.log(results);
+});
     })
-// This is for pushing data to the database
-// app.post("/scrape", function(req,res){
-//     for(let i = 0;i<article.length;i ++){
-//         db.Article.create({
-//             title:title[i],
-//             link:link[i]
-//         })
-//     }
-// })
-
     app.get("/articles", function (req, res) {
         // TODO: Finish the route so it grabs all of the articles
         db.Article.find({})
--- server.js
+++ server.js
@@ -2,11 +2,6 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
@@ -27,34 +22,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
+apiRoutes = require("./routes/apiRoutes")
+apiRoutes(app)
 mongoose.connect("mongodb://localhost/onionScraperDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-axios.get("https://www.theonion.com/").then(function(response) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("article").each(function(i, element) {
-
-    var title = $(element).children().text();
-    var link = $(element).find("a").attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title,
-      link
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
