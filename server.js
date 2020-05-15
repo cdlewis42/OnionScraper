619e523ab6f7863c8d5d45ef6b1764bf67393558
Celeste Lewis <cdlewis42@gmail.com>
2020-05-15 17:04:13 -0400

    added process.env.PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var mongoose = require("mongoose");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
